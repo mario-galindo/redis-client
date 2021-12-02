e9572e5b44bfe48a053b455d8da8531636d8a112
mario-galindo <mgalindo9410@gmail.com>
2021-12-02 17:37:06 -0600

    the most basic node js api added

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-const express = require('express')
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+
+app.get("/photos", async (req, res) => {
+  const albumId = req.query.albumId;
+  const { data } = await axios.get(
+    "https://jsonplaceholder.typicode.com/photos",
+    { params: { albumId } }
+  );
+  res.json(data);
+});
+
+app.listen(3000, () => {
+  console.log("listening at http://localhost:3000");
+});
