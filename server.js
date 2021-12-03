675b0a69de685f2f24b11fed57001a4d918cbbc6
mario-galindo <mgalindo9410@gmail.com>
2021-12-03 14:21:04 -0600

    caching added to api response

--- server.js
+++ server.js
@@ -4,24 +4,30 @@ const cors = require("cors");
 const Redis = require("redis");
 
 const redisClient = Redis.createClient();
+redisClient.connect();
+
 const app = express();
 app.use(cors());
 
 app.get("/photos", async (req, res) => {
-  await redisClient.connect();
-  
-  const value = await redisClient.get("name")
 
-  const albumId = req.query.albumId;
-  const { data } = await axios.get(
-    "https://jsonplaceholder.typicode.com/photos",
-    { params: { albumId } }
-  );
+  const photosCache = await redisClient.get("photos");
 
-  redisClient.setEx("photos", 3600, JSON.stringify(data));
+  if (photosCache != null) {
+    console.log("Cache hit!");
+    return res.json(JSON.parse(photosCache));
+  } else {
+    console.log("Cache Miss");
+
+    const albumId = req.query.albumId;
+    const { data } = await axios.get(
+      "https://jsonplaceholder.typicode.com/photos",
+      { params: { albumId } }
+    );
+
+    redisClient.setEx("photos", 3600, JSON.stringify(data));
+  }
   res.json(data);
-  console.log(value);
-
 });
 
 app.get("/photos/:id", async (req, res) => {
