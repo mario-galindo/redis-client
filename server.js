9f7f53e941eea87ee622f2e079957f4a2330e1fd
mario-galindo <mgalindo9410@gmail.com>
2021-12-03 18:33:08 -0600

    node js + redis cache finished

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const Redis = require("redis");
 
 const redisClient = Redis.createClient();
+const DEFAULT_EXPIRATION = 3600;
 redisClient.connect();
 
 const app = express();
@@ -20,16 +21,28 @@ app.get("/photos", async (req, res) => {
       { params: { albumId } }
     );
 
-    redisClient.setEx("photos", 3600, JSON.stringify(data));
+    redisClient.setEx("photos", DEFAULT_EXPIRATION, JSON.stringify(data));
     res.json(data);
   }
 });
 
 app.get("/photos/:id", async (req, res) => {
-  const { data } = await axios.get(
-    `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
-  );
-  res.json(data);
+  const photo = await redisClient.get(`photo:${req.params.id}`);
+
+  if (photo != null) {
+    return res.json(JSON.parse(photo));
+  } else {
+    const { data } = await axios.get(
+      `https://jsonplaceholder.typicode.com/photos/${req.params.id}`
+    );
+
+    redisClient.setEx(
+      `photo:${req.params.id}`,
+      DEFAULT_EXPIRATION,
+      JSON.stringify(data)
+    );
+    res.json(data);
+  }
 });
 
 app.listen(3000, () => {
